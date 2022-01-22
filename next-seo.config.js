ce9ec6bb688e1b046c526b757d36c5b4947d63bc
Daria Diachkova <daria.diachkova@mail.ru>
2022-01-22 20:58:21 +0100

    feat(seo-config): update

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,26 +1,29 @@
+import { APP_PRODUCTION } from 'constants/locations'
+import { TEXTS } from 'constants/texts'
+
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "nextarter-chakra",
-  titleTemplate: "%s | nextarter-chakra",
-  defaultTitle: "nextarter-chakra",
-  description: "Next.js + chakra-ui + TypeScript template",
-  canonical: "https://nextarter-chakra.sznm.dev",
+  title: TEXTS.app_title,
+  titleTemplate: '%s',
+  defaultTitle: TEXTS.app_title,
+  description: TEXTS.app_description,
+  canonical: APP_PRODUCTION,
   openGraph: {
-    url: "https://nextarter-chakra.sznm.dev",
-    title: "nextarter-chakra",
-    description: "Next.js + chakra-ui + TypeScript template",
+    url: APP_PRODUCTION,
+    title: TEXTS.app_title,
+    description: TEXTS.app_description,
     images: [
       {
-        url: "https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
-        alt: "nextarter-chakra.sznm.dev og-image",
+        url: `${APP_PRODUCTION}/assets/og-image.png`,
+        alt: `${TEXTS.app_title} og-image`,
       },
     ],
-    site_name: "nextarter-chakra",
+    site_name: TEXTS.app_title,
   },
   twitter: {
-    handle: "@sozonome",
-    cardType: "summary_large_image",
+    handle: '@_DariaDear',
+    cardType: 'summary_large_image',
   },
-};
+}
 
-export default defaultSEOConfig;
+export default defaultSEOConfig
