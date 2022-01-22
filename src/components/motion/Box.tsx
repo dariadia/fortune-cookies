9b4ccd561bdccd42acea13aba745a87b10912f88
Daria Diachkova <daria.diachkova@mail.ru>
2022-01-22 12:09:26 +0100

    feat(types): add

--- src/components/motion/Box.tsx
+++ src/components/motion/Box.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { chakra, HTMLChakraProps } from '@chakra-ui/react'
 import { motion, HTMLMotionProps } from 'framer-motion'
 
-import { Merge } from 'types/merge'
+import { Merge } from 'types'
 
 type MotionBoxProps = Merge<HTMLChakraProps<'div'>, HTMLMotionProps<'div'>>
 
---
+++ types/index.d.ts
@@ -0,0 +1,7 @@
+export * from './Date'
+export * from './Event'
+export * from './FortuneCookie'
+export * from './Locale'
+export * from './Merge'
+export * from './Page'
+export * from './Request'
