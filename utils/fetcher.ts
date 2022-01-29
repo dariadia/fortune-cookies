110e4ec3f53e53fb2238c9898c446d7833cbb11d
Daria Diachkova <daria.diachkova@mail.ru>
2022-01-29 21:57:17 +0100

    feat(utils): extend

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,2 +1,20 @@
+import { RequestOption, RequestOptions } from 'types'
+
 export const fetcher = (url: string): Promise<Response> =>
   fetch(url).then(res => res.json())
+
+export const getProtocol = (host: string): string =>
+  host.includes('localhost') ? 'http://' : 'https://'
+
+export const buildRequestUrl = (
+  url: string,
+  RequestParams?: RequestOptions,
+): string => {
+  if (!RequestParams) {
+    return url
+  }
+  const queryString = Object.keys(RequestParams)
+    .map(key => `${key}=${RequestParams[key as RequestOption]}`)
+    .join('&')
+  return `${url}?${queryString}`
+}
