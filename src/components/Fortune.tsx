3be4994261db3f5cedbcf1ac2ad286fd2ee2d0f3
Daria Diachkova <daria.diachkova@mail.ru>
2022-03-07 18:34:01 +0100

    feat(Fortune): alter styling

--- src/components/Fortune.tsx
+++ src/components/Fortune.tsx
@@ -63,7 +63,8 @@ export const Message: React.FC<MessageProps> = props => {
         <Box className="fortune-cookie_source">
           <a href={source_link} target="_blank">
             {source_title}
-          </a>{' '}
+          </a>
+          <br />
           {source_author}
         </Box>
       </MotionBox>
